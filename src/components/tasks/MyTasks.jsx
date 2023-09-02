d74659a01f40d16d072809529a7d9b7ce5a5d1be
emon1995 <emonhossain995@gmail.com>
2023-09-02 17:21:45 +0600

    my task modal open

--- src/components/tasks/MyTasks.jsx
+++ src/components/tasks/MyTasks.jsx
@@ -2,14 +2,22 @@ import {
   CheckIcon,
   DocumentMagnifyingGlassIcon,
 } from '@heroicons/react/24/outline';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { userTask } from '../../redux/features/tasks/tasksSlice';
+import { updateStatus, userTask } from '../../redux/features/tasks/tasksSlice';
+import TaskDetailsModal from './TaskDetailsModal';
 
 const MyTasks = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const [taskId, setTaskId] = useState(0);
   const { tasks, userSpecificTask } = useSelector(state => state.tasksSlice);
   const { name: userName } = useSelector(state => state.userSlice);
   const dispatch = useDispatch();
+
+  const handleModal = id => {
+    setTaskId(id);
+    setIsOpen(!isOpen);
+  }
 
   useEffect(() => {
     dispatch(userTask(userName));
@@ -17,6 +25,7 @@ const MyTasks = () => {
 
   return (
     <div>
+      <TaskDetailsModal isOpen={isOpen} setIsOpen={setIsOpen} taskId={taskId} />
       <h1 className="text-xl my-3">My Tasks</h1>
       <div className=" h-[750px] overflow-auto space-y-3">
         {
@@ -27,10 +36,10 @@ const MyTasks = () => {
             >
               <h1>{item.title}</h1>
               <div className="flex gap-3">
-                <button className="grid place-content-center" title="Details">
+                <button onClick={() => handleModal(item.id)} className="grid place-content-center" title="Details">
                   <DocumentMagnifyingGlassIcon className="w-5 h-5 text-primary" />
                 </button>
-                <button className="grid place-content-center" title="Done">
+                <button onClick={() => dispatch(updateStatus({ id: item.id, status: "done" }))} className="grid place-content-center" title="Done">
                   <CheckIcon className="w-5 h-5 text-primary" />
                 </button>
               </div>
